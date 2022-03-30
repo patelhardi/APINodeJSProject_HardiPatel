b6916d2e16ba18fa413b439d0a37b59248fc35d9
Hardi Hemantkumar Patel <patelhardi5@gmail.com>
2022-03-30 17:08:44 -0400

    AddAPIProject

---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+Used APIs: https://docs.repliers.io/docs/repliers-developer-api/ZG9jOjMwOTc2MjE5-introduction
+           https://maps.googleapis.com/maps/api/staticmap
+
+Some limitations using repliers real estate api        
+https://docs.repliers.io/docs/repliers-developer-api/ZG9jOjIyMjM4OTM2-getting-started
+
+For cities such as north york, missisauga, edmonton does not have any data in the api
+For city Richmond Hill need to write city name Richmond Hill if you write richmondhill without space between it won't show any data.
+
---
+++ index.js
@@ -0,0 +1,94 @@
+//IMPORT REQUIRED MODULES
+const express = require("express");
+const path = require("path");
+const fs = require("fs"); //file r/w module built-in to Node.js
+const axios = require("axios");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+//set up Express object
+const app = express(); //app is the Express object
+const port = process.env.PORT || "8888";
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+//CSS and client-side JS are static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//test Express to see if it's working
+/*app.get("/", (req, res) => {
+  res.status(200).send("Hello...");
+});*/
+
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home Page" });
+});
+
+//set up some page routes
+app.get("/listings", (req, res) => {
+    let cityname = req.query["name"];
+    displayList(cityname, res);
+});
+
+app.get("/details/:num", (req, res) => {
+  //console.log(req.params.num);
+  let mlsNum = req.params.num;
+  displayDetails(mlsNum, res);
+});
+
+//set up server listening
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
+
+//function to display cities
+function displayList(cname, res) {
+    var pageData = {
+      title: "List of Available Homes",
+      list: null
+    };
+    axios(
+      //the request
+      {
+        url: "https://sandbox.repliers.io/listings?city=" + cname,
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json', 
+          'REPLIERS-API-KEY': 'SKoKOGhEO42QzdkZ1cowKgLGm2mwm4'
+        }
+      }
+    ).then(function (response){
+      //on success do stuff
+      //console.log(response.data.listings);
+      pageData.list = response.data.listings; //store JSON results in pageData.list (previously null)
+      res.render("listings", pageData);
+    }).catch(function (error){
+      console.log(error);
+    });
+  }
+
+  function displayDetails(mnum, res) {
+    var pageData = {
+      title: "Details about Home",
+      detail: null
+    };
+    axios(
+      //the request
+      {
+        url: "https://sandbox.repliers.io/listings/" + mnum,
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json', 
+          'REPLIERS-API-KEY': 'SKoKOGhEO42QzdkZ1cowKgLGm2mwm4'
+        }
+      }
+    ).then(function (response){
+      //on success do stuff
+      //console.log(response.data);
+      pageData.detail = response.data; //store JSON results in pageData.list (previously null)
+      res.render("details", pageData);
+    }).catch(function (error){
+      console.log(error);
+    });
+  }
